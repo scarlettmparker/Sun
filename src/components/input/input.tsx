aa4a72949a1a736843831c16bcc92e8b41e0c47c
Scarlett Parker <95775118+scarlettmparker@users.noreply.github.com>
2025-10-05 00:08:54 +0100

    Style range input boxes

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,12 +1,34 @@
+import styles from "./input.module.css";
+
 type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 /**
  * Scarlet UI Input.
  */
 const Input = (props: InputProps) => {
-  const { ...rest } = props;
+  const { type, ...rest } = props;
 
-  return <input {...rest} />;
+  switch (type) {
+    case "range":
+      // TODO: don't know what the pattern for this is, check other UI libraries
+      return (
+        <input
+          {...rest}
+          type="range"
+          className={`${rest.className} ${styles.range}`}
+        />
+      );
+    case "checkbox":
+      return (
+        <input
+          {...rest}
+          type="checkbox"
+          className={`${rest.className} ${styles.checkbox}`}
+        />
+      );
+    default:
+      return <input {...rest} type={type} />;
+  }
 };
 
 export default Input;
