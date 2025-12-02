2fdb0a3ac441abb8e3c8875a2f5d158e27c0ed57
Scarlett Parker <95775118+scarlettmparker@users.noreply.github.com>
2025-12-02 22:49:57 +0000

    Ignore coverage folder in linter

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
-    ignores: ["dist/**", "node_modules/**", "config.js"],
+    ignores: ["dist/**", "node_modules/**", "config.js", "coverage/**"],
   },
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
