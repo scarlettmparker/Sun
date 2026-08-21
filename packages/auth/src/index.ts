c9aba88748bd2012d3e01af1f255bdcdab4a39cb
scarlettmparker <scarwe2020@gmail.com>
2026-08-21 18:13:49 +0100

    Revert "Move logic to auth package"
    This reverts commit 42ec24d4c483099bedf67fc09e9ff27d42381cdf.

--- packages/auth/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { getCookieValue } from "@sun/api";
-export * from "./cookies.js";
-export * from "./client-store.js";
-export * from "./oauth.js";
--- packages/auth/src/server.ts
+++
@@ -1,3 +0,0 @@
-export * from "./auth-helpers.js";
-export * from "./routes.js";
-export { getCookieValue } from "@sun/api";
